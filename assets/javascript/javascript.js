f14187052001cc5a1557589bc98a94aa6e242bd3
Blair Brownlie <bbrownli@asu.edu>
2019-03-26 23:38:22 -0700

    making sure what little I have is saved

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,14 +1,23 @@
 $(function() {
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
-  var search = "rick+and+morty"
+  var search = "rick+and+morty";
   var apiKey = "&api_key=7kDTo2sTD91Ik3wn177f3iUtDZAPeUvy&limit=10";
   $.ajax({
-    url: queryURL+ search + apiKey,
+    url: queryURL + search + apiKey,
     method: "GET"
   }).then(function(response) {
-    var gifs = response.data;    
+    var gifs = response.data;
     console.log(gifs);
-    
-    
+    var gifDiv = $("#gifs");
+    for (let i = 0; i < gifs.length; i++) {
+      if (gifs.ratings !== "r" && gifs.ratings !== "pg-13") {
+        var stillImgUrl = gifs[i].images.fixed_height_still.url;
+        console.log(stillImgUrl);
+
+        var newGif = $("<img class='card-img-top'>");
+        newGif.attr("src", stillImgUrl);
+        gifDiv.append(newGif);
+      }
+    }
   });
 });
