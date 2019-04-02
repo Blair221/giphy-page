4ad6c0c4b44c66b91387cb405bb304cbf9a7fdca
Blair Brownlie <bbrownli@asu.edu>
2019-04-02 00:22:51 -0700

    I can get my original buttons to generate the gifs I want, but I'm having trouble getting my new buttons to display their gifs

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,53 +1,65 @@
 $(function() {
-  var animeArray = ["luffy", "naruto", "goku", "Deku"];
+  var animeArray = ["luffy", "naruto", "goku", "deku"];
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
-  var search = $("input")
-    .val()
-    .trim();
+  var search = $(this).attr('data-anime');
   var apiKey = "&api_key=7kDTo2sTD91Ik3wn177f3iUtDZAPeUvy&limit=10";
 
   var createButtons = function() {
     $("#anime-buttons").empty();
-    for (let i = 0; i < animeArray.length; i++) {
+    for (var i = 0; i < animeArray.length; i++) {
       var newCharacter = $("<button class='mx-2 btn-outline-danger'>");
+      newCharacter.addClass("anime-button");
+      newCharacter.attr('data-anime', animeArray[i]);
       newCharacter.text(animeArray[i]);
       $("#anime-buttons").append(newCharacter);
     }
-    $('input').text('');
+    $("input").text("");
   };
   $("#submit-button").on("click", function() {
     event.preventDefault();
-    var search = $("input")
+    var inputValue = $("input")
       .val()
       .trim();
-    if (animeArray.includes(search)) {
-      alert("This anime character already has a button!")
-    }
-    else if ((search !== "") && (search.length > 1)) {
-      animeArray.push(search);
+    if (animeArray.includes(inputValue)) {
+      alert("This anime character already has a button!");
+    } else if (inputValue !== "" && inputValue.length > 1) {
+      animeArray.push(inputValue);
     }
 
     createButtons();
-    
   });
-  createButtons();
-  $.ajax({
-    url: queryURL + search + apiKey,
-    method: "GET"
-  }).then(function(response) {
-    var gifs = response.data;
-    console.log(gifs);
-    var gifDiv = $("#gifs");
-    for (let i = 0; i < gifs.length; i++) {
-      if (gifs.ratings !== "r" && gifs.ratings !== "pg-13") {
-        var animatedImgUrl = gifs[i].images.fixed_width.url;
-        console.log(animatedImgUrl);
 
-        var newGif = $("<img class='card-img-top'>");
-        newGif.attr("src", animatedImgUrl);
-        newGif.attr('data-state', 'animated');
-        gifDiv.append(newGif);
-      }
-    }
+  createButtons();
+  $(".anime-button").on("click", function() {
+    $("#gifs").empty();
+    $('.anime-button').removeClass('active');
+    $(this).addClass('active');
+    var search = $(this).attr('data-anime');
+    $.ajax({
+      url: queryURL + search + apiKey,
+      method: "GET"
+    }).then(function(response) {
+      
+      var gifs = response.data;
+
+      console.log(gifs);
+      
+
+      var gifDiv = $("#gifs");
+
+      for (var i = 0; i < gifs.length; i++) {
+        var animatedImgUrl = gifs[i].images.fixed_width.url;
+        var stillImgUrl = gifs[i].images.fixed_width_still.url;
+        
+          
+          
+
+          var newGif = $("<img class='card-img-top'>");
+          newGif.attr("src", animatedImgUrl);
+          newGif.attr("data-state", "animated");
+          gifDiv.append(newGif);
+        }
+      
+    });
   });
 });
