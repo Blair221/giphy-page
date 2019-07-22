977655edfdc5ef5b4dec8e42290a7ad85ceff6da
Blair Brownlie <bbrownli@asu.edu>
2019-07-21 17:31:26 -0700

    adding the last pieces of functionality

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,7 @@
 $(function() {
   var animeArray = ["luffy", "naruto", "goku", "deku"];
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
-  var search = $(this).attr('data-anime');
+  var search = $(this).attr("data-anime");
   var apiKey = "&api_key=7kDTo2sTD91Ik3wn177f3iUtDZAPeUvy&limit=10";
 
   var createButtons = function() {
@@ -9,12 +9,13 @@ $(function() {
     for (var i = 0; i < animeArray.length; i++) {
       var newCharacter = $("<button class='mx-2 btn-outline-danger'>");
       newCharacter.addClass("anime-button");
-      newCharacter.attr('data-anime', animeArray[i]);
+      newCharacter.attr("data-anime", animeArray[i]);
       newCharacter.text(animeArray[i]);
       $("#anime-buttons").append(newCharacter);
     }
     $("input").text("");
   };
+
   $("#submit-button").on("click", function() {
     event.preventDefault();
     var inputValue = $("input")
@@ -25,41 +26,51 @@ $(function() {
     } else if (inputValue !== "" && inputValue.length > 1) {
       animeArray.push(inputValue);
     }
-
+    inputValue = "";
     createButtons();
   });
 
   createButtons();
-  $(".anime-button").on("click", function() {
+
+  $(document).on("click", ".anime-button", function() {
     $("#gifs").empty();
-    $('.anime-button').removeClass('active');
-    $(this).addClass('active');
-    var search = $(this).attr('data-anime');
+    $(".anime-button").removeClass("active");
+    $(this).addClass("active");
+    var search = $(this).attr("data-anime");
     $.ajax({
       url: queryURL + search + apiKey,
       method: "GET"
     }).then(function(response) {
-      
       var gifs = response.data;
 
       console.log(gifs);
-      
 
       var gifDiv = $("#gifs");
 
       for (var i = 0; i < gifs.length; i++) {
         var animatedImgUrl = gifs[i].images.fixed_width.url;
         var stillImgUrl = gifs[i].images.fixed_width_still.url;
-        
-          
-          
 
-          var newGif = $("<img class='card-img-top'>");
-          newGif.attr("src", animatedImgUrl);
-          newGif.attr("data-state", "animated");
-          gifDiv.append(newGif);
-        }
-      
+        var newGif = $("<img class='card-img-top'>");
+        newGif.attr("src", animatedImgUrl);
+        newGif.attr("data-animated", animatedImgUrl);
+        newGif.attr("data-still", stillImgUrl);
+        newGif.attr("data-state", "animated");
+        newGif.addClass("anime-image");
+        gifDiv.append(newGif);
+      }
     });
   });
+});
+
+$(document).on("click", ".anime-image", function() {
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animated"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
 });
